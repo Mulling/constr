c2ac9ba214ed9b38351acb0717717fbac4ea3b29
Mulling <lmulling@proton.me>
2024-02-03 16:50:01 -0300

    Proof of concept

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,20 +1,26 @@
 use constr::constr;
 
-// TODO: also generate a macro to_str!(FOO);
-
 #[constr(to_str(u8))]
 pub mod foo {
-    #[to("foo")]
+    #[str = "foo"]
     pub const FOO: usize = 0x01;
 
-    #[to("bar")]
+    #[str = "bar"]
     pub const BAR: usize = 0x02;
 
-    #[to("baz")]
+    #[str = "baz"]
     pub const BAZ: usize = 0x03;
+
+    #[str = "TODO: This shoudl fail as we already have 0x03"]
+    pub const SHOULD_FAIL: usize = 0x03;
+
+    // FIXME: should be IGNORED
+    // pub const IGNORED: u8 = 0x00;
 }
 
 #[test]
-fn constr_valid() {
-    println!("{}", foo::to_str(10));
+fn test_mod_foo() {
+    assert_eq!(to_str(3), "baz");
+    assert_eq!(to_str(2), "bar");
+    assert_eq!(to_str(1), "foo");
 }
